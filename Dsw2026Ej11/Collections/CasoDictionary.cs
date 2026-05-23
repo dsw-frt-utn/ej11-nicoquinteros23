403d4e813826dac99dd7935f5396f828d2a2a3b5
Nicolas <nicoquinteros.nq@gmail.com>
2026-05-23 11:00:09 -0300

    clase linq y list

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -3,14 +3,16 @@
 using Dsw2026Ej11.Domain;
 using System.Collections.Generic;
 
-//Crear un diccionario donde la clave sea el legajo y el valor el alumno
-//Incluir un método para agregar un alumno al diccionario
-//Incluir un método para buscar un alumno utilizando la clave
-//Incluir un método para retornar el diccionario
-//Incluir un método para eliminar un alumno utilizando la clave
+// Crear un diccionario donde la clave sea el legajo y el valor el alumno.
+// En esta clase se usa Alumno.Id como legajo.
 public class CasoDictionary
 {
-	private readonly Dictionary<int, Alumno> _alumnos = new();
+	private readonly Dictionary<int, Alumno> _alumnos;
+
+	public CasoDictionary()
+	{
+		_alumnos = new Dictionary<int, Alumno>();
+	}
 
 	public void AgregarAlumno(Alumno alumno)
 	{
